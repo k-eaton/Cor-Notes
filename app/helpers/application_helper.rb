fc2a5809808cc19dc1c61fc30ee7638c9da5bbd8
k-eaton <katrina.eaton@gmail.com>
2016-10-07 11:35:31 -0700

    automate page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return appropriate title if not specified
+  def full_title(page_title = '')
+    base_title = "Cor-Notes"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
