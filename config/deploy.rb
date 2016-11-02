0a07d8041350c695cdc39b38c390213869cacd2e
k-eaton <katrina.eaton@gmail.com>
2016-11-02 11:50:25 -0700

    setup Capistrano pt7

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.6.1'
 
-server "www.cor-notes.com", roles (:app, :web, :db), :primary => true
+server "www.cor-notes.com", roles {:app, :web, :db}, :primary => true
 
 set :repo_url, 'https://github.com/k-eaton/Cor-Notes.git'
 set :application, 'cor-notes'
