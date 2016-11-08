aadccae561a1862f6d3ff6b1aa3b0cdbad50b71d
k-eaton <katrina.eaton@gmail.com>
2016-11-08 09:45:23 -0800

    change to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'help',    to: 'static_pages#help', controller: 'static_pages'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  # get '/signup',  to: 'users#new'
+  get '/signup',  to: 'users#new'
   # resources :users
 
 end
