3bb9d7032bec7fa0058b66d2f1d5936a430c74ef
k-eaton <katrina.eaton@gmail.com>
2016-11-09 10:02:23 -0800

    routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # get 'static_pages#help'
   # get '/static_pages/help',    to: 'static_pages#help'
-  get '/static_pages/:help' => 'static_pages#show'
+  get '/static_pages/:page' => 'static_pages#help'
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
